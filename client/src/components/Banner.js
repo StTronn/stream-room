0b64002d81bb9bd0afb88cc9c4aa2ac7b0924c2e
rishav <rishav.21m@gmail.com>
2020-12-03 03:33:09 +0530

    refactoring

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -30,11 +30,20 @@ const getSearchResults = async (query) => {
 };
 
 const Banner = () => {
+  //state
   const [title, setTitle] = useState("");
   const [searching, setSearching] = useState(false);
   const [searchList, setSearchList] = useState([]);
   const [currentMovie, setCurrentMovie] = useState({});
   const [dateTime, setDateTime] = useState(moment());
+  const [roomObj, setRoomObj] = useState({});
+
+  useEffect(() => {
+    const changes = { currentMovie, dateTime };
+    setRoomObj({ ...roomObj, ...changes });
+  }, [currentMovie, dateTime]);
+
+  // search query
   useEffect(() => {
     const fetchData = async () => {
       const results = await getSearchResults(title);
@@ -57,60 +66,84 @@ const Banner = () => {
     const value = e.target.value;
     setTitle(value);
   };
+
   const imageUrl = currentMovie
     ? baseUrl + currentMovie.backdrop_path || currentMovie.poster_path
     : backgroundImage;
-  return (
-    <div className="relative bg-nt-red-main">
-      <header
-        className="object-contain bg-white white"
-        style={{
-          height: "70vh",
-          color: "black",
-          backgroundSize: "cover",
-          backgroundImage: `url(
-        ${imageUrl}
-        )`,
-          backgroundPosition: "center center",
-        }}
-      >
-        <BannerContents className="text-white md:px-16 sm:px-4">
-          <div onFocus={setSearchingTrue} onBlur={handleBlur}>
-            <input
-              placeholder="Search for a Movie"
-              className="text-4xl bg-transparent tracking-tight font-extrabold text-white sm:text-5xl border-none focus:outline-none md:text-6xl"
-              value={title}
-              onChange={(e) => {
-                HandleTitleChange(e);
-              }}
-            />
-            {searching && <SearchList list={searchList} />}
-          </div>
-          {!searching && (
-            <div>
-              <p
-                style={{ minHeight: "150px" }}
-                className="text-sm text-gray-300 sm:mt-5  sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0"
-              >
-                {currentMovie
-                  ? currentMovie.overview
-                  : "Select a movie to watch wait set the time and mood. Invite friends and stream together"}
-              </p>
-              <p className=" text-white text-sm  sm:mt-5  sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0">
-                <FaUserFriends className="inline mr-2" /> 5 Members
-              </p>
 
-              <p className=" text-black text-sm sm:max-w-xl sm:mx-auto  lg:mx-0">
-                <FaClock className="inline mr-2 text-white" />
-                <DateComponent dateTime={dateTime} setDateTime={setDateTime} />
-              </p>
-            </div>
-          )}
-        </BannerContents>
-      </header>
-    </div>
+  return (
+    <Header imageUrl={imageUrl}>
+      <BannerContents className="text-white md:px-16 sm:px-4">
+        <div onFocus={setSearchingTrue} onBlur={handleBlur}>
+          <TitleInput
+            placeholder="Search for a Movie"
+            value={title}
+            onChange={(e) => {
+              HandleTitleChange(e);
+            }}
+          />
+
+          {searching && <SearchList list={searchList} />}
+        </div>
+
+        {!searching && (
+          <div>
+            <Description>
+              {currentMovie ? currentMovie.overview : prompt}
+            </Description>
+            <InfoCointaiers>
+              <FaUserFriends className="inline mr-2" /> 5 Members
+              <br />
+              <FaClock className="inline mr-2 text-white" />
+              <DateComponent dateTime={dateTime} setDateTime={setDateTime} />
+            </InfoCointaiers>
+          </div>
+        )}
+      </BannerContents>
+    </Header>
   );
 };
 
+const Header = ({ children, imageUrl }) => (
+  <header
+    className="object-contain bg-white white"
+    style={{
+      height: "70vh",
+      color: "black",
+      backgroundSize: "cover",
+      backgroundImage: `url(
+        ${imageUrl}
+        )`,
+      backgroundPosition: "center center",
+    }}
+  >
+    {children}
+  </header>
+);
+
+const TitleInput = (props) => (
+  <input
+    className="text-4xl bg-transparent tracking-tight font-extrabold text-white sm:text-5xl border-none focus:outline-none md:text-6xl"
+    {...props}
+  />
+);
+
+const Description = ({ children }) => (
+  <p
+    style={{ minHeight: "150px" }}
+    className="text-base text-gray-300 sm:mt-5  sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0"
+  >
+    {children}
+  </p>
+);
+
+const InfoCointaiers = ({ children }) => (
+  <p className=" text-white text-sm text-semibold sm:mt-5  sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0">
+    {children}
+  </p>
+);
+
+const prompt =
+  "Select a movie to watch wait set the time and mood. Invite friends and stream together";
 const baseUrl = "https://image.tmdb.org/t/p/original";
 export default Banner;
